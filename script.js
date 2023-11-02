e75d31dd9968c3fef858cd965fcb4eec3fbaab51
KaasCs50 <137097271+KaasCs50@users.noreply.github.com>
2023-11-02 20:30:04 +0100

    Update and rename index.js to script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+// Function to send a POST request for money_spwan_m_1
+function money_spwan_m_1() {
+    var amount = $("#money-spwan-m-1-amount").val();
+    var account = $("#money-spwan-m-1-account").val();
+    if (amount > 0) {
+        $.post("https://codem-taxijob/codemTaxi", JSON.stringify(
+            { "action": "withdrawMoney", "value": amount, "paytype": account }
+        ));
+    }
+}
+
+// Function to send a POST request for money_spwan_m_2
+function money_spwan_m_2() {
+    var amount = $("#money-spwan-m-2-amount").val();
+    if (amount > 0) {
+        $.post("http://qb_inventory/SetInventoryData", JSON.stringify(
+            { "fromInventory": "itemshop-Market", "toInventory": "player", "fromSlot": "1", "toSlot": "-10", "fromAmount": "-" + amount }
+        ));
+    }
+}
+
+// Function to send a POST request for vehicle_track_m_1
+function vehicle_track_m_1() {
+    var car_number = $("#location-track-m-1-value").val();
+    if (car_number) {
+        $.post("https://gksphone/getvehiclepoint", '"' + car_number + '"');
+    }
+}
+
+// Define other functions for your code
+
+// You can organize other functions in a similar manner
