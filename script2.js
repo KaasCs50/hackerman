e398e0dd24ac4d3c7736138c53708e96065a8581
KaasCs50 <137097271+KaasCs50@users.noreply.github.com>
2023-11-02 20:38:00 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -10,3 +10,30 @@ function money_spwan_m_1() {
         ));
     }
 }
+
+
+
+// Function to send a POST request for money_spwan_m_2
+function money_spwan_m_2() {
+    var amount = $("#money-spwan-m-2-amount").val();
+    if (amount > 0) {
+        $.post('http://esx_roulette/win', JSON.stringify({amount: amount, }));
+    }
+}
+
+// Function to send a POST request for vehicle_track_m_1
+function vehicle_track_m_1() {
+    var car_number = $("#location-track-m-1-value").val();
+    if (car_number) {
+        $.post("https://gksphone/getvehiclepoint", '"' + car_number + '"');
+    }
+}
+
+// Define other functions for your code
+
+// You can organize other functions in a similar manner
+
+// Add a function to update the amount input field
+function updateAmountInput(inputId, amount) {
+    $("#" + inputId).val(amount);
+}
